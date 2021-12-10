8c3a62086b2613f501c45107b9764a301ed1d495
Huseyn Mammadov <huseynmv@yahoo.com>
2021-12-10 16:07:57 +0400

    view for vendor app

--- vendor/views.py
+++ vendor/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def vendor(request):
+    return render(request, 'vendor.html')
+
+def single_vendor(request):
+    return render(request, 'vendor-single.html')
