4c1c9408fd73fe5976aa5c6c45078571fc258185
Huseyn Mammadov <huseynmv@yahoo.com>
2021-12-10 15:55:16 +0400

    view for pages app

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+def cart(request):
+    return render(request, 'cart.html')
+
+def error(request):
+    return render(request, 'error-404.html')
+
+def vendor(request):
+    return render(request, 'become-a-vendor.html')
+
+def compare(request):
+    return render(request, 'compare.html')
+
+def faq(request):
+    return render(request, 'faq.html')
+
+def order(request):
+    return render(request, 'order.html')
+
+def order_view(request):
+    return render(request, 'order-view.html')
+
+def wishlist(request):
+    return render(request, 'wishlist.html')
+
+def checkout(request):
+    return render(request, 'checkout.html')
