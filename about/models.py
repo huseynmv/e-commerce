d62a6e6aa3f76965669851649a4a44617e01bc9a
Huseyn Mammadov <huseynmv@yahoo.com>
2021-12-15 19:25:56 +0400

    Leaders table created

--- about/models.py
+++ about/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Leaders(models.Model):
+    image = models.ImageField(upload_to='leaders/')
+    name = models.CharField(max_length=127)
+    title = models.CharField(max_length=127)
+    social_media = models.URLField(null=True, blank=True)
+    
+    class Meta:
+        verbose_name = 'Leader'
+        
+    def __str__(self):
+        return f'{self.name}'
