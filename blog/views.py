f22e6489781b90f9ea80ab13affbaeb6acaeb022
Huseyn Mammadov <huseynmv@yahoo.com>
2021-12-16 15:12:04 +0400

    Blog added from database

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
 def blog(request):
-    return render(request, 'blog.html')
+    blog = Blog.objects.all()
+    context = {
+        'blog' : blog
+    }
+    return render(request, 'blog.html', context)
 
 def single_blog(request):
     return render(request, 'single-blog.html')
