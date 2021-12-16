13b731e8cee222444839c14c8f19ef466a9f8264
Huseyn Mammadov <huseynmv@yahoo.com>
2021-12-16 15:01:30 +0400

    Blog model added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    category = models.CharField(max_length=127, blank=True, null=True)
+    name = models.CharField(max_length=127, blank=True, null=True)
+    image = models.ImageField(upload_to='blog/')
+    author = models.CharField(max_length=127, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True, null=True,blank=True)
+    link = models.URLField(max_length=255)
+    
+    
+    def __str__(self):
+        return f'{self.name}'
