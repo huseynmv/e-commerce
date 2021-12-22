0db8b324cfe7b08f3d7e53ee4ec3b8caae9f484b
Huseyn Mammadov <huseynmv@yahoo.com>
2021-12-22 18:21:48 +0400

    blog detail view

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import  single_blog, BlogListView
+from .views import  BlogDetailView, BlogListView
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='blog'),
-    path('blog-details/', single_blog, name='blog-details'),
+    path('<int:pk>', BlogDetailView.as_view(), name='blog-details'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Blog
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # class BlogListView(ListView):
 #     pass
@@ -18,5 +18,15 @@ class BlogListView(ListView):
     template_name = 'blog.html'
     paginate_by = 3
 
-def single_blog(request):
-    return render(request, 'single-blog.html')
+# def single_blog(request):
+#     return render(request, 'single-blog.html')
+
+class BlogDetailView(DetailView):
+    model = Blog
+    template_name = 'single-blog.html'
+    context_object_name='blog_obj'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['name'] = 'Test user'
+        return context
