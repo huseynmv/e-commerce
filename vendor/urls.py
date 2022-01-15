050b85f6e854f933910164c7245cc47d54a500c2
Huseyn Mammadov <huseynmv@yahoo.com>
2022-01-15 20:42:20 +0400

    name&namespace for base.html

--- vendor/urls.py
+++ vendor/urls.py
@@ -4,6 +4,6 @@ from .views import *
 app_name = 'pages'
 
 urlpatterns = [
-    path('', vendor ,name='vendor'),
+    path('', vendor , name='vendor'),
     path('single-vendor/', single_vendor ,name='single-vendor'),
 ]
