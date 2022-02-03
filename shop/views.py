1e8658b36ed3557dac26f3aa1cf0a54dde03839b
Huseyn Mammadov <huseynmv@yahoo.com>
2022-02-03 13:39:09 +0400

    static product deleted from template

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from . models import Product
 # Create your views here.
 def product(request):
-    return render(request, 'product.html')
+    product = Product.objects.all()
+    context = {
+        'product' : product
+    }
+    return render(request, 'product.html', context)
 
 def single_product(request):
     return render(request, 'single-product.html')
