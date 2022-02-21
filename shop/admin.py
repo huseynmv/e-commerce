b1f5ddc13f5588b584df5c1251bcad50e5937fcc
Huseyn Mammadov <huseynmv@yahoo.com>
2022-02-21 16:52:56 +0400

    slug fiel

--- shop/admin.py
+++ shop/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from . models import Product, Order, OrderItem, ProductCategory
 # Register your models here.
 
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    fields = ('category', 'desc', 'name', 'price', 'image',)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(ProductCategory)
--- shop/urls.py
+++ shop/urls.py
@@ -4,7 +4,7 @@ from .views import checkout, product, update_item,cart, search, ProductDetailVie
 app_name = 'shop'
 
 urlpatterns = [
-    path('<int:pk>', ProductDetailView.as_view(), name='single-product'),
+    path('<slug:slug>', ProductDetailView.as_view(), name='single-product'),
     path('', product, name='product'),
     path('checkout/', checkout, name='checkout'),
     path('cart/', cart, name='cart'),
