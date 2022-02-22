cdd72d69ad6d7c035533f152febf45fecb7eaa4e
Huseyn Mammadov <huseynmv@yahoo.com>
2022-02-23 00:48:31 +0400

    comment model registered

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Blog, BlogCategory
+from .models import Blog, BlogCategory, Comment
 # Register your models here.
 
 admin.site.register(Blog)
+admin.site.register(Comment)
 admin.site.register(BlogCategory)
