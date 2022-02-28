8ab44d5703f28bf85f5c09ca63a2cb693a0d4438
Huseyn Mammadov <huseynmv@yahoo.com>
2022-02-28 15:03:46 +0400

    wishlist url

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import checkout, product, update_item,cart, search, ProductDetailView, filter, filter_data
+from .views import checkout, product, update_item,cart, search, ProductDetailView, filter, filter_data, wishlist
 
 app_name = 'shop'
 
@@ -9,6 +9,7 @@ urlpatterns = [
     path('checkout/', checkout, name='checkout'),
     path('cart/', cart, name='cart'),
     path('update_item/', update_item, name='update_item'),
+    path('wishlist/', wishlist, name='update_item'),
     path('cart/update_item/', update_item, name="update_item"),
     path('search/', search, name='search'),
     path('filter/<slug:slug>', filter, name='filter'),
