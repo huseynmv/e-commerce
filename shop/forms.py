6e49ba874a0f26833e3642a93b95ed488dfcc911
Huseyn Mammadov <huseynmv@yahoo.com>
2022-03-03 00:24:19 +0400

    checkout form update

--- shop/forms.py
+++ shop/forms.py
@@ -4,18 +4,16 @@ from .models import Checkout
 class CheckoutForm(forms.ModelForm):
     class Meta:
         model = Checkout
-        fields = ('adress1', 'adress2', 'town', 'zip', 'phone', 'state',)
+        fields = ('adress1', 'adress2', 'town', 'zip', 'phone',)
         widgets = {
             'adress1': forms.TextInput(
                 attrs={'class': 'form-control form-control-md ', 'placeholder': 'House number and street name'}),
-            'adress2': forms.Textarea(
+            'adress2': forms.TextInput(
                 attrs={'class': 'form-control form-control-md ', 'placeholder': 'Apartment, suite, unit, etc. (optional)'}),
             'town': forms.TextInput(
                 attrs={'class': 'form-control form-control-md', 'placeholder': 'Town/City'}),
             'zip': forms.TextInput(
                 attrs={'class': 'form-control form-control-md', 'placeholder': 'ZIP code'}),
-            'state': forms.TextInput(
-                attrs={'class': 'form-control form-control-md', 'placeholder': 'Street adress'}),
             'phone': forms.TextInput(
                 attrs={'class': 'form-control form-control-md', 'placeholder': 'Phone Number'}),
             
