76724a452519e8b5012bf8b72a44f16e88d15fcf
Huseyn Mammadov <huseynmv@yahoo.com>
2022-03-02 13:26:05 +0400

    wishlist_view

--- shop/views.py
+++ shop/views.py
@@ -138,27 +138,26 @@ def wishlist(request):
 
 
 def wishlist_view(request):
-    
+        
     if request.user.is_authenticated:
         user = request.user
-        order, created = Order.objects.get_or_create(user=user, status=False)
-        items = order.orderitem_set.all()
-        cartItems = order.get_cart_items
+        wishlist, created = Wishlist.objects.get_or_create(user=user, status=False)
+        items = wishlist.wishlistitem_set.all()
+        # cartItems = order.get_cart_items
         
     else:
         items = []
-        order = {'get_cart_total': 0,'get_cart_items': 0 }
-        cartItems = order['get_cart_items']
+        # order = {'get_cart_total': 0,'get_cart_items': 0 }
+        # cartItems = order['get_cart_items']
         
         
         
     context = {
         'items': items,
-        'order': order,
-        'cartItems': cartItems
+        # 'order': order,
+        # 'cartItems': cartItems
     }
     return render(request, 'wishlist.html', context)
-
 
 def search(request):
     if request.method == 'POST':
