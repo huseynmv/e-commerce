3c30e5a3e5dd8cf975579f60a4a4bf4d004521e3
Huseyn Mammadov <huseynmv@yahoo.com>
2022-03-11 19:15:45 +0400

    HomeSecondarySlider registered

--- home/admin.py
+++ home/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
-from .models import HomeSlider
+from .models import HomeSlider, HomeSecondarySlider
 
 # Register your models here.
 
 @admin.register(HomeSlider)
 class HomeSliderAdmin(admin.ModelAdmin):
     fileds = ('title_az','title_en', 'desc_az', 'desc_en', 'image',)
-
+    
+    
+admin.site.register(HomeSecondarySlider)
