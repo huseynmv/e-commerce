d8659c4a988cc02e3a591f563a39645d79293fe2
Huseyn Mammadov <huseynmv@yahoo.com>
2022-03-11 18:42:37 +0400

    Home Slider model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class HomeSlider(models.Model):
+    image = models.ImageField(upload_to='home/slider/')
+    title = models.CharField(max_length=200, null=True,blank=True)
+    desc = models.CharField(max_length=200,null=True, blank=True)
