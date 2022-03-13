f9938731927bcac6c60029f2a10953900665f115
Huseyn Mammadov <huseynmv@yahoo.com>
2022-03-13 13:55:24 +0400

    Product discount from admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
+from math import ceil
 from . models import Product, Order, OrderItem, ProductCategory, Color, Brand, Wishlist, WishlistItem, Checkout
 # Register your models here.
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     fields = ('category','brand', 'price', 'image','image2','image3','image4','image5','size','color', 'name_az','name_en','desc_en', 'desc_az')
+    actions = ['discount_30']
+    
+    def discount_30(self,request, queryset):
+        
+        discount = 30  # percentage
+
+        for product in queryset:
+            """ Set a discount of 30% to selected products """
+            multiplier = discount / 100  
+            old_price = product.price
+            new_price = ceil(old_price - (old_price * multiplier))
+            product.price = new_price
+            product.save(update_fields=['price'])
+    discount_30.short_description = 'Set 30%% discount'
     
 @admin.register(ProductCategory)
 class ProductCategoryAdmin(admin.ModelAdmin):
